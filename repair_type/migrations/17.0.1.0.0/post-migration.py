9ffab6722cdbd29f0f997cd17415afc39722c0ea
Lois Rilo <lois.rilo@forgeflow.com>
2025-02-21 11:49:10 +0100

    fixup! [FIX] repair_type: migration script

--- repair_type/migrations/17.0.1.0.0/post-migration.py
+++ repair_type/migrations/17.0.1.0.0/post-migration.py
@@ -6,16 +6,35 @@ from openupgradelib import openupgrade
 
 @openupgrade.migrate()
 def migrate(env, version):
+    if not openupgrade.table_exists(env.cr, "repair_type"):
+        return
+    openupgrade.logged_query(
+        env.cr,
+        """
+        ALTER TABLE stock_picking_type
+        ADD COLUMN old_repair_type_id_legacy INTEGER;
+        """,
+    )
     openupgrade.logged_query(
         env.cr,
         """
         INSERT INTO stock_picking_type
         (create_date, default_location_dest_id, default_remove_location_dest_id,
-        name,default_add_location_src_id, default_remove_location_src_id)
+        name,default_add_location_src_id, default_remove_location_src_id,
+        old_repair_type_id_legacy)
         SELECT create_date, destination_location_add_part_id,
-        destination_location_remove_part_id, name::jsonb,
-        source_location_add_part_id, source_location_remove_part_id
+        destination_location_remove_part_id, jsonb_build_object('en_US', name) as name,
+        source_location_add_part_id, source_location_remove_part_id, id
         FROM repair_type;
+        """,
+    )
+    openupgrade.logged_query(
+        env.cr,
+        """
+            UPDATE repair_order ro
+            SET picking_type_id = spt.id
+            FROM stock_picking_type spt
+            WHERE ro.repair_type_id = spt.old_repair_type_id_legacy;
         """,
     )
     openupgrade.logged_query(
